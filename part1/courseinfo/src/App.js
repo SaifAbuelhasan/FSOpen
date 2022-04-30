3648d16f4ae91429053b3c1718ed3d46ae162c0f
Saif <saiofa99@gmail.com>
2022-04-30 20:00:12 +0300

    Finished part1 courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {
+        props.parts.map((element, i) => {
+          return (
+            <Part key={i} name={element.name} exercises={element.exercises} />
+          )
+        })
+      }
+    </>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.map(element => element.exercises).reduce((prev,curr) => {
+    return prev + curr
+  }, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App;
